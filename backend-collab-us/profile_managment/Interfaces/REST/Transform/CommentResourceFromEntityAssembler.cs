50421b8e66527c6211beb2f7c544bb24a0b5baf0
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:44:26 -0500

    feat(comments): add CommentResourceFromEntityAssembler for resource mapping

---
+++ backend-collab-us/profile_managment/Interfaces/REST/Transform/CommentResourceFromEntityAssembler.cs
@@ -0,0 +1,19 @@
+﻿using backend_collab_us.comment_managment.domain.model.agregates;
+using backend_collab_us.profile_managment.Interfaces.REST.Resources;
+
+namespace backend_collab_us.profile_managment.Interfaces.REST.Transform;
+
+public static class CommentResourceFromEntityAssembler
+{
+    public static CommentResource ToResourceFromEntity(Comment comment)
+    {
+        return new CommentResource(
+            comment.Id,
+            comment.ProfileId,
+            comment.UserId,
+            comment.Rating,
+            comment.CommentText,
+            comment.CreatedAt
+        );
+    }
+}
