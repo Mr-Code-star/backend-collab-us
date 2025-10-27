c93ad261ba838ef0636881708720dffd85e640c0
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:40:59 -0500

    feat(comments): add ICommentRepository interface with profile-based query

---
+++ backend-collab-us/profile_managment/domain/repositories/ICommentRepository.cs
@@ -0,0 +1,9 @@
+﻿using backend_collab_us.comment_managment.domain.model.agregates;
+using backend_collab_us.Shared.Domain.Repositories;
+
+namespace backend_collab_us.profile_managment.domain.repositories;
+
+public interface ICommentRepository : IBaseRepository<Comment>
+{
+    Task<IEnumerable<Comment>> FindByProfileIdAsync(int profileId);
+}
