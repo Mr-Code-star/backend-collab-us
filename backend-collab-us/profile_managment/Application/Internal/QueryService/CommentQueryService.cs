e8da182c7cfcc9dd8c83b48b734f3511cd9b0e48
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:43:15 -0500

    feat(comments): implement CommentQueryService for profile comment retrieval

---
+++ backend-collab-us/profile_managment/Application/Internal/QueryService/CommentQueryService.cs
@@ -0,0 +1,14 @@
+﻿using backend_collab_us.comment_managment.domain.model.agregates;
+using backend_collab_us.profile_managment.domain.model.queries;
+using backend_collab_us.profile_managment.domain.repositories;
+using backend_collab_us.profile_managment.domain.services;
+
+namespace backend_collab_us.comment_managment.Application.Internal.QueryService;
+
+public class CommentQueryService(ICommentRepository commentRepository) : ICommentQueryService
+{
+    public async Task<IEnumerable<Comment>> Handle(GetCommentsByProfileIdQuery query)
+    {
+        return await commentRepository.FindByProfileIdAsync(query.ProfileId);
+    }
+}
