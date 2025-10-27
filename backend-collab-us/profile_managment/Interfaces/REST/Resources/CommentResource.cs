95dd485fdab343bfe85ae97b3846d0b0b91d0092
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:43:48 -0500

    feat(comments): add CommentResource DTO

---
+++ backend-collab-us/profile_managment/Interfaces/REST/Resources/CommentResource.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_collab_us.profile_managment.Interfaces.REST.Resources;
+
+public record CommentResource(
+    int Id,
+    int ProfileId,
+    int UserId,
+    int Rating,
+    string Comment,
+    DateTime CreatedAt
+);
