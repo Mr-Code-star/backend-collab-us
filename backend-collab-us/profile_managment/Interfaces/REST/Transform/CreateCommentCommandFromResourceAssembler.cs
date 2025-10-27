9dcc805290f9a754eb712ba84b44471c9308bc47
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:44:39 -0500

    feat(comments): add CreateCommentCommandFromResourceAssembler for command transformation

---
+++ backend-collab-us/profile_managment/Interfaces/REST/Transform/CreateCommentCommandFromResourceAssembler.cs
@@ -0,0 +1,17 @@
+﻿using backend_collab_us.profile_managment.domain.model.commands;
+using backend_collab_us.profile_managment.Interfaces.REST.Resources;
+
+namespace backend_collab_us.profile_managment.Interfaces.REST.Transform;
+
+public class CreateCommentCommandFromResourceAssembler
+{
+    public static CreateCommentCommand ToCommandFromResource(CreateCommentResource resource)
+    {
+        return new CreateCommentCommand(
+            resource.ProfileId,
+            resource.UserId,
+            resource.Rating,
+            resource.Comment
+        );
+    }
+}
