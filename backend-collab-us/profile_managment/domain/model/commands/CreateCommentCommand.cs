a3d57cbe027adb24bbf7a3792d0965bc9647112b
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:40:03 -0500

    feat(comments): add CreateCommentCommand for comment creation

---
+++ backend-collab-us/profile_managment/domain/model/commands/CreateCommentCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace backend_collab_us.profile_managment.domain.model.commands;
+
+public record CreateCommentCommand(
+    int ProfileId,
+    int UserId,
+    int Rating,
+    string Comment
+);
