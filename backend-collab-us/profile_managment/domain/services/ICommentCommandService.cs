52b0cbdf3da84594e2c734f292f3faa495c127ce
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:41:48 -0500

    feat(comments): add ICommentCommandService for comment creation handling

---
+++ backend-collab-us/profile_managment/domain/services/ICommentCommandService.cs
@@ -0,0 +1,9 @@
+﻿using backend_collab_us.comment_managment.domain.model.agregates;
+using backend_collab_us.profile_managment.domain.model.commands;
+
+namespace backend_collab_us.profile_managment.domain.services;
+
+public interface ICommentCommandService
+{
+    Task<Comment?> Handle(CreateCommentCommand command);
+}
