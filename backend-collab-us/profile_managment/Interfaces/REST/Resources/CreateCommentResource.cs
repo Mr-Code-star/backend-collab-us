270d2821d7225f210fcfef6060d6dfd11aa02c5e
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:44:09 -0500

    feat(comments): add CreateCommentResource for API input

---
+++ backend-collab-us/profile_managment/Interfaces/REST/Resources/CreateCommentResource.cs
@@ -0,0 +1,8 @@
+﻿namespace backend_collab_us.profile_managment.Interfaces.REST.Resources;
+
+public record CreateCommentResource(
+    int ProfileId,
+    int UserId,
+    int Rating,
+    string Comment
+);
