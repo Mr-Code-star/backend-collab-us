0ab60bb678ce33eb42b6aaf302f4228496ff3297
Mr-Code-start <vitalybaca92@gmail.com>
2025-10-26 20:42:04 -0500

    feat(comments): add ICommentQueryService for comment query handling

---
+++ backend-collab-us/profile_managment/domain/services/ICommentQueryService.cs
@@ -0,0 +1,10 @@
+﻿using backend_collab_us.comment_managment.domain.model.agregates;
+using backend_collab_us.profile_managment.domain.model.queries;
+
+namespace backend_collab_us.profile_managment.domain.services;
+
+public interface ICommentQueryService
+{
+    Task<IEnumerable<Comment>> Handle(GetCommentsByProfileIdQuery query);
+
+}
