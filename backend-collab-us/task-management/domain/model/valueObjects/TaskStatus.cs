102c710aaf2dbf432c01f6c32714a0e296732c44
Mr-Code-start <vitalybaca92@gmail.com>
2025-11-06 18:58:11 -0500

    feat(valueObject): add new valueObject TaskStatus.cs

---
+++ backend-collab-us/task-management/domain/model/valueObjects/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace backend_collab_us.task_management.domain.model.valueObjects;
+
+public class TaskStatus
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+    public bool Active { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    
+    public static readonly string PENDING = "pending";
+    public static readonly string COMPLETED = "completed";
+    public static readonly string DELAYED = "retrasado";
+}
