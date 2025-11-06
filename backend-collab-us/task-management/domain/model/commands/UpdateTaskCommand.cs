d5566b63baae637d8f66aa7d1ae0707ddfb101c6
Mr-Code-start <vitalybaca92@gmail.com>
2025-11-06 18:56:22 -0500

    feat(command): add new command UpdateTaskCommand

---
+++ backend-collab-us/task-management/domain/model/commands/UpdateTaskCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace backend_collab_us.task_management.domain.model.commands;
+
+public record UpdateTaskCommand(
+    int TaskId,
+    int ProjectId,
+    string Title,
+    string Description,
+    DateTime DueDate,
+    string Status,
+    string Priority,
+    int AssignedTo,
+    string AssignedToName,
+    string Role,
+    string Comment,
+    int EstimatedHours,
+    int ActualHours
+);
